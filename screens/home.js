066085e392a4ab89ee265f4ea41c3ce14f5bbb21
raziqali0306 <raziqali0306@gmail.com>
2020-04-02 00:45:12 +0530

    asynchronous storage

--- screens/home.js
+++ screens/home.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, StyleSheet, Button, AsyncStorage} from 'react-native';
 
-const Home = ({navigation}) => {
-  const pressHandler = () => {
-    navigation.push('ReviewDetails');
+const Home = props => {
+  const [num, setNum] = useState(0);
+
+  const getCountFromStorage = async () => {
+    const countString = await AsyncStorage.getItem('clickCount');
+    console.log('------- got count string as ', countString);
+    setNum(Number(countString));
+  };
+  useEffect(() => {
+    getCountFromStorage();
+  }, []);
+
+  const tapHandler = async () => {
+    setNum(numOne => {
+      const x = numOne + 1;
+      AsyncStorage.setItem('clickCount', `${x}`);
+      return x;
+    });
   };
 
   return (
     <View style={styles.mainViewStyle}>
       <Text>Home screen</Text>
-      <Button title={'goto Review Details->'} onPress={pressHandler} />
+      {/* <Button title={'goto Review Details->'} onPress={gotoReviewDetails} /> */}
+      <Text>num = {num}</Text>
+      <Button title={'TAP'} onPress={tapHandler} />
     </View>
   );
 };
